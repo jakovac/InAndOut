057d894f791e5758dd104ef070fb4599239631e8
Milan Jakovac <jakovac_milan@hotmail.com>
2021-08-27 13:04:46 +0200

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string todaysDate = DateTime.Now.ToShortDateString();
+            return View();
+
+            //return Ok(todaysDate);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = " + id);
+        }
+    }
+}
---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,174 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using InAndOut.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ExpenseController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+
+            foreach(var obj in objList)
+            {
+                obj.ExpenseType = _db.ExpenseTypes.FirstOrDefault(u => u.Id == obj.ExpenseTypeId);
+            }
+
+            return View(objList);
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            //IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+            //{
+            //    Text = i.ExpenseTypeName,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.TypeDropDown = TypeDropDown;
+
+            //return View();
+            ExpenseVM expenseVM = new ExpenseVM()
+            {
+                Expense = new Expense(),
+                TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+                {
+                    Text = i.ExpenseTypeName,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            return View(expenseVM);
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExpenseVM obj)
+        {
+            if(ModelState.IsValid)
+            {
+                //obj.ExpenseTypeId = 1;
+                _db.Add(obj.Expense);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET-Delete
+        public IActionResult Delete(int? id)
+        {
+           
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            var expenseVM = new ExpenseVM()
+            {
+                Expense = obj,
+                TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+                {
+                    Text = i.ExpenseTypeName,
+                    Value = i.Id.ToString()
+                })
+
+            };
+
+            return View(expenseVM);
+        }
+
+        //POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(ExpenseVM objList)
+        {
+           
+            var obj = _db.Expenses.Find(objList.Expense.Id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        //GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            var objList = new ExpenseVM()
+            {
+                Expense = obj,
+                TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+                {
+                    Text = i.ExpenseTypeName,
+                    Value = i.Id.ToString()
+                })
+
+            };
+
+            //IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+            //{
+            //    Text = i.ExpenseTypeName,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.TypeDropDown = TypeDropDown;
+            return View(objList);
+        }
+
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ExpenseVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj.Expense);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+    }
+}
